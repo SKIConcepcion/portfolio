dd9b99e5524c34e6fd9bddf9b0c2bc514b07b651
SKIConcepcion <skiconcepcion@gmail.com>
2024-06-21 20:09:07 +0800

    feat: added header and grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,11 +25,41 @@ function App() {
 
 
       <div className="grid-container">
-        <div className="grid-item">1</div>
-        <div className="grid-item">2</div>
-        <div className="grid-item">3</div>
-        <div className="grid-item">4</div>
-        <div className="grid-item">5</div>
+        <div className="grid-item-1">
+          <div className="grid-item-1-line"></div>
+            <h4 className="grid-item-1-title"> About Me </h4>
+          <div className="grid-item-1-container"></div>
+
+          <div className="grid-item-3-line"></div>
+            <h4 className="grid-item-3-title"> Contact </h4>
+          <div className="grid-item-3-container"></div>
+        </div>
+
+        <div className="grid-item-2">
+          <div className="grid-item-2-line"></div>
+          <h4 className="grid-item-2-title"><span>MOBILE APP PROJECT</span>  Dito Nlng </h4>
+          <div className="grid-item-2-container"></div>
+        </div>
+
+        
+        <div className="grid-item-4">
+          <div className="grid-item-4-line"></div>
+          <h4 className="grid-item-4-title"><span>WEBSITE PROJECT</span> Brics </h4>
+          <div className="grid-item-4-container"></div>
+
+          
+          <div className="grid-item-5-line"></div>
+            <h4 className="grid-item-5-title"> Peronal Information </h4>
+          <div className="grid-item-5-container"></div>
+        </div>
+
+        <div className="grid-item-6">
+          <div className="grid-item-6-line"></div>
+          <h4 className="grid-item-6-title"> Peronal Information </h4>
+          <div className="grid-item-6-container"></div>
+        </div>
+
+
       </div>
 
 
